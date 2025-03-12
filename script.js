96f9262dcf74f1bac85092e72784683f590cac53
qw1te0v <111095553+qw1te0v@users.noreply.github.com>
2025-03-12 20:48:38 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
     const kzBtn = document.getElementById("kz-btn");
     const ruBtn = document.getElementById("ru-btn");
+    const enBtn = document.getElementById("en-btn");
 
 
     const headerlist1 = document.getElementById("headerlist1");
     const headerlist2 = document.getElementById("headerlist2");
+    const headerlist3 = document.getElementById("headerlist3");
     const title = document.getElementById("title");
     const subtitle = document.getElementById("subtitle");
     const moreBtn = document.getElementById("more-btn");
@@ -20,32 +22,53 @@ document.addEventListener("DOMContentLoaded", () => {
         setLanguage("ru");
     });
 
+    enBtn.addEventListener("click", () => {
+        setLanguage("en");
+    });
+
     function setLanguage(lang) {
         localStorage.setItem("lang", lang);
 
         if (lang === "kz") {
             kzBtn.classList.add("active");
             ruBtn.classList.remove("active");
+            enBtn.classList.remove("active");
 
 
 
             headerlist1.textContent = "БІЗ ТУРАЛЫ";
             headerlist2.textContent = "ДЕМЕУШІЛЕР";
+            headerlist3.textContent = "БЛОГ";
             title.textContent = "SENIM.KZ - қысқа әрі, есте қаларлық";
             subtitle.textContent = "БІЗГЕ ТАПСЫРЫС БЕРІҢІЗ!";
             moreBtn.textContent = "ТОЛЫҒЫРАҚ ↗";
         } 
         
         
-        else {
+        else if (lang === "ru") {
             ruBtn.classList.add("active");
             kzBtn.classList.remove("active");
+            enBtn.classList.remove("active");
 
             headerlist1.textContent = "О НАС";
             headerlist2.textContent = "СПОНСОРЫ";
+            headerlist3.textContent = "БЛОГ";
             title.textContent = "SENIM.KZ - кратко и запоминающийся";
             subtitle.textContent = "ЗАКАЖИТЕ У НАС!";
             moreBtn.textContent = "ПОДРОБНЕЕ ↗";
+        }
+
+        else if (lang === "en") {
+            enBtn.classList.add("active");
+            kzBtn.classList.remove("active");
+            ruBtn.classList.remove("active");
+
+            headerlist1.textContent = "ABOUT US";
+            headerlist2.textContent = "SPONSORS";
+            headerlist3.textContent = "BLOG";
+            title.textContent = "SENIM.KZ - short and memorable";
+            subtitle.textContent = "ORDER FROM US!";
+            moreBtn.textContent = "MORE ↗";
         }
     }
 });
